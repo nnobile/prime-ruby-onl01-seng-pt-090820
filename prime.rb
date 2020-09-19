5027f1e197e11ac09154478e067f55d7f7a69c79
Nick Nobile <nickcnobile@gmail.com>
2020-09-19 20:25:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,10 @@
-def prime?(integer)
-  integer(1..20).to_a  
+# How do you create a range of numbers?
+#(1..52)
+
+# How do you turn a range into an arrauy so that it can be iterated over?
+#(1..52).to_a
+
+def prime?(num)
+  if num < 2
+    false
 end
